20bf0b60f9f7097304820ade2df1510690dc75ca
Nic <68457714+np531@users.noreply.github.com>
2021-07-13 03:18:56 +0000

    Add files via upload

---
+++ vim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# == 1 ]
+then
+	if [ $1 == "new" ]
+	then
+		echo "Installing vim..."
+		sudo pacman -S vim git
+		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	   	wget https://raw.githubusercontent.com/np531/setup/main/.vimrc  ~/.vimrc
+	fi
+fi
+	echo "Downloading plugins..."
+	vim +PluginInstall +qall
