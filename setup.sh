0759e4f1a8349214ae451b8c2badc10736cd0b47
Nic <68457714+np531@users.noreply.github.com>
2026-03-01 20:57:20 +1100

    Simplify setup script by removing distro checks
    Removed distribution detection and conditional package installation. Simplified the script to always update and install specified packages using APT.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-# -f to install linux reqs
 
-distribution=$(cat /etc/*-release | uniq -u | head -n 1 | grep -Eo "[^=]*$")
+# Designed for APT, update this line if using different package manager
+sudo apt -y update && sudo apt -y upgrade
+sudo apt install vim-gtk3 git eza -y
 
-if [ "$1" != "-f" ] ; then
-	echo "=== Installing uequired packages, updating system... ==="
-	if [ "$distribution" == "Ubuntu" ] ; then
-		sudo apt -y update
-		sudo apt -y upgrade
-		sudo apt-get install vim-gtk3 git exa -y
-	elif [ "$distribution" == "Arch" ] ; then
-		sudo pacman -S gvim git exa
-	else 
-		echo "Can't detect distro, skipping package installs..."
-		echo "Please manually install gvim, git and exa from your distro's package manager"
-	fi
-fi
 
 echo "=== Pulling dotfiles from repo ==="
 curl https://raw.githubusercontent.com/np531/setup/main/.vimrc > ~/.vimrc
